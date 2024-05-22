5930ee4f2e2a946b377a1fafea9a97b93c72ceb8
blankCubicle <138397220+blankCubicle@users.noreply.github.com>
2024-05-22 19:15:43 -0400

    Improve input handling and implement single-round play

--- app.js
+++ app.js
@@ -4,29 +4,63 @@ function getComputerChoice() {
 
   switch (randomNumber) {
     case 1:
-      return 'Rock';
+      return 'rock';
     case 2:
-      return 'Paper';
+      return 'paper';
     case 3:
     default:
-      return 'Scissors';
+      return 'scissors';
   }
 }
 
-function getHumanChoice() {
-  const humanChoice = prompt('What is your choice?', '');
+function getHumanChoice(message = 'What is your choice?') {
+  let humanInput = prompt(message, '');
 
   // human pressed 'Cancel' button on prompt
-  if (humanChoice === null) return 'canceled';
+  if (humanInput === null) return 'cancel';
 
-  switch (humanChoice.toLowerCase()) {
+  humanInput = humanInput.toLowerCase();
+
+  switch (humanInput) {
     case 'rock':
     case 'paper':
     case 'scissors':
-      return humanChoice;
-    case '':
-      return 'nothing';
+      return humanInput;
     default:
-      return 'invalid';
+      return getHumanChoice(
+        "I didn't get that, try again? (rock/paper/scissors)",
+      );
   }
 }
+
+const rules = {
+  rock: { beatenBy: 'paper' },
+  paper: { beatenBy: 'scissors' },
+  scissors: { beatenBy: 'rock' },
+};
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 'cancel') return console.log('Round canceled :(');
+
+  console.log(`You chose ${humanChoice}`);
+  console.log(`Computer chose ${computerChoice}`);
+
+  if (humanChoice === computerChoice) {
+    return console.log(`<== It's a tie! ==>`);
+  }
+
+  if (rules[humanChoice].beatenBy === computerChoice) {
+    computerScore += 1;
+    console.log(
+      `<== You lose :( — ${humanChoice} is beaten by ${computerChoice} ==>`,
+    );
+  } else {
+    humanScore += 1;
+    console.log(`<== You WON! — ${humanChoice} beats ${computerChoice} ==>`);
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
